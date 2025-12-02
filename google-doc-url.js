de1b6e027131d872e8d56b88a3bfd5f10a2a1bf1
Arooj98 <fatia852@newschool.edu>
2025-12-02 15:21:48 -0500

    hopefully Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1XsQvEX73Y4aEYCYJENTFzUzCn1v20AgIUQcoMtM8tTQ/edit?gid=0#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSdEh1f8UPSVG5Jn0VysdGRy8V0XlBJtzKksKxK4dDB4Wv0OC2tQ5Fm38ZkxwbFM_iW18YB4Ug4ngtc/pubhtml';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
